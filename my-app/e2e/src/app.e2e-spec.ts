15b77b7badbe44da6f348a3020c70344d102d567
Rakesh <rakesh.golkonda@navaratan.com>
2019-11-27 16:33:21 +0530

    Added proctractor

--- my-app/e2e/src/app.e2e-spec.ts
+++ my-app/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,32 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  // let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+  // beforeEach(() => {
+  //   page = new AppPage();
+  // });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-app app is running!');
+    // page.navigateTo();
+    browser.get('http://localhost:4200/login');
+    browser.sleep(3000);
+    element(by.css('[formcontrolname="userName"]')).sendKeys('test');
+    browser.sleep(3000);
+    element(by.css('[formcontrolname="password"]')).sendKeys('test');
+    browser.sleep(3000);
+    element(by.buttonText('Submit')).click();
+    browser.sleep(3000);
+    // expect(page.getTitleText()).toEqual('my-app app is running!');
+    
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  // afterEach(async () => {
+  //   // Assert that there are no errors emitted from the browser
+  //   const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+  //   expect(logs).not.toContain(jasmine.objectContaining({
+  //     level: logging.Level.SEVERE,
+  //   } as logging.Entry));
+  // });
 });
